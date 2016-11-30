f1dd119da28a3bff2383c38b299a7b2b693d6b24
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-11-30 12:25:33 -0800

    clean up code, change how the class headerText is used

--- nav_bootstrap.php
+++ nav_bootstrap.php
@@ -12,64 +12,31 @@ $rootpath = $_SERVER['DOCUMENT_ROOT'];
 ?>
 
 <div class="headerContainer">
-    <!-- <div class='desktop-burger'><i class='icon-menu-1'></i></div> -->
     <div class="navWidthWrapper">
-
-
         <div class="logoWrapper">
             <a class="logoLink" href="preorder/index.php">
                 <img class="navLogo" src="../images/VJ_logo.svg" alt="Virgil James" />
             </a>
         </div>
-
-        
         <div class="rightItemsContainer">
             <div class="desktopLinksContainer">
                 <div class="textLinksContainer">
                     <a href="preorder/index.php">Shop</a>
                     <a href="preorder/about.php">About</a>
                 </div>
-
-                
-
-                <!-- <div id="CartNavContainer" class="iconWrapper cartIconWrapper cartDropdownWrapper"> -->
-                    <?php 
-                        //include $rootpath.'/incs/navCart.php'; 
-                    ?>
-                <!-- </div> -->
                 <a class="iconWrapper" href="javascript:goToCheckout();">
                     <i class="icon-basket"></i>
                 </a>           
-
-
-                <!-- <div class="iconWrapper userDropdownWrapper"> -->
-                    <?php 
-                        //include $rootpath.'/incs/navUser.php'; 
-                    ?>
-                <!-- </div> -->
                 <a class="iconWrapper" href="preorder/login.php">
                     <i class="icon-torso"></i>
                 </a>
-
             </div>
-            
-
             <div class='burgerContainer'>
                 <i class='icon-menu-1'></i>
             </div>
-
-
-
         </div>
-
-
         <div class="bottomBorder">
         </div>
-
-
     </div>
 </div>
-
-<!-- <div class="navPlaceholder"></div> -->
-
 <div class="scrollWaypoint"></div>
