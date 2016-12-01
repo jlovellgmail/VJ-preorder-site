c32e5d0d796c27bbbcbe27e9ed52e1a730921703
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-12-01 15:37:48 -0800

    make dropdown animated, and push the bottom border down

--- nav_bootstrap.php
+++ nav_bootstrap.php
@@ -13,64 +13,74 @@ $rootpath = $_SERVER['DOCUMENT_ROOT'];
 
 <div class="headerContainer">
     <div class="navWidthWrapper">
-        <div class="logoWrapper">
-            <a class="logoLink" href="preorder/index.php">
-                <img class="navLogo" src="../images/VJ_logo.svg" alt="Virgil James" />
-            </a>
-        </div>
-        <div class="rightItemsContainer">
-            <div class="desktopLinksContainer">
-                <div class="textLinksContainer">
-                    <a href="preorder/index.php">Shop</a>
-                    <a href="preorder/about.php">About</a>
-                </div>
-                <a class="iconWrapper" href="javascript:goToCheckout();">
-                    <i class="icon-basket"></i>
-                </a>           
-                <a class="iconWrapper" href="preorder/login.php">
-                    <i class="icon-torso"></i>
+
+
+        <div class="headerHeightWrapper">
+            <div class="logoWrapper">
+                <a class="logoLink" href="preorder/index.php">
+                    <img class="navLogo" src="../images/VJ_logo.svg" alt="Virgil James" />
                 </a>
             </div>
-            <div class='burgerContainer'>
-                <i class='icon-menu-1'></i>
-            </div>
-
-
-            
-            <div class="dropdown">
-                <div class="dropdownLinksContainer">
+            <div class="rightItemsContainer">
+                <div class="desktopLinksContainer">
                     <div class="textLinksContainer">
                         <a href="preorder/index.php">Shop</a>
                         <a href="preorder/about.php">About</a>
-
-                        
-                        <!--
-                        <a href="javascript:goToCheckout();">Cart</a>
-                        <a href="preorder/login.php">Account</a>
-                        -->
-                        <a class="iconWrapper" href="javascript:goToCheckout();">
-                            <i class="icon-basket"></i>
-                        </a>           
-                        <a class="iconWrapper" href="preorder/login.php">
-                            <i class="icon-torso"></i>
-                        </a>
-
-
                     </div>
+                    <a class="iconWrapper" href="javascript:goToCheckout();">
+                        <i class="icon-basket"></i>
+                    </a>           
+                    <a class="iconWrapper" href="preorder/login.php">
+                        <i class="icon-torso"></i>
+                    </a>
+                </div>
+                <div class='burgerContainer'>
+                    <i class='icon-menu-1'></i>
                 </div>
             </div>
-            <script>
-                jQuery(function($){
-                    $( '.icon-menu-1' ).click(function(){
-                        $('.dropdown').toggleClass('visible')
-                    });
-                });
-            </script>
-
-
-
 
         </div>
+
+
+
+
+
+        
+        <div class="dropdown">
+            <div class="dropdownLinksContainer">
+                <div class="textLinksContainer">
+                    <a href="preorder/index.php">Shop</a>
+                    <a href="preorder/about.php">About</a>
+
+                    
+                    <!--
+                    <a href="javascript:goToCheckout();">Cart</a>
+                    <a href="preorder/login.php">Account</a>
+                    -->
+                    <a class="iconWrapper" href="javascript:goToCheckout();">
+                        <i class="icon-basket"></i>
+                    </a>           
+                    <a class="iconWrapper" href="preorder/login.php">
+                        <i class="icon-torso"></i>
+                    </a>
+
+
+                </div>
+            </div>
+        </div>
+        <script>
+            jQuery(function($){
+                $( '.icon-menu-1' ).click(function(){
+                    $('.dropdown').toggleClass('visible')
+                });
+            });
+        </script>
+
+
+
+
+
+
         <div class="bottomBorder">
         </div>
     </div>
