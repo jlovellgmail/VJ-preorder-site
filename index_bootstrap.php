7beee4ee2d62bd6e6af3155057a43c8cb4329c5b
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-12-02 15:00:23 -0800

    put media query after the class instead of before, and something else happened to index_bootstrap

--- index_bootstrap.php
+++ index_bootstrap.php
@@ -5,13 +5,16 @@ $seo_variable = "home";
 ?>
 <html class="no-js" lang="en">
 <head>
-    <?php include '/incs/head-links.php'; ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="../css/bootstrap-grid.css" />
+    <?php include '/incs/head-links_bootstrap.php'; ?>
 	<link rel="stylesheet" href="/css/index.css" />
-	<link rel="stylesheet" href="../css/preorder.css" />
+	<link rel="stylesheet" href="../css/preorder_bootstrap.css" />
 </head>
 <body class="body">
-	<?php include 'nav.php'; ?>
-	
+	<?php include 'nav_bootstrap.php'; ?>
+
+
 
 	<div class="preorderPage">
 		<div class="landingFrame">
@@ -61,7 +64,9 @@ $seo_variable = "home";
 	</div>
 
 
+
+
+
 	<?php include "/incs/footer.php"; ?>
 </body>
 </html>
-
