de184a2071f7a37e96aefcf0b8c1b28a164ee2fe
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-12-02 13:17:46 -0800

    add new downloaded fontello css file to my site files

--- incs/head-links_bootstrap2.php
+++ incs/head-links_bootstrap2.php
@@ -4,7 +4,10 @@
 <script src="/js/vendor/modernizr.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="/js/general.js"></script>
-<link rel="stylesheet" href="/css/fontello.css">
+
+<!-- JL -->
+<link rel="stylesheet" href="../css/fontello.css">
+
 <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet'>
 <link href='https://fonts.googleapis.com/css?family=Gloria+Hallelujah' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="/css/normalize.css">
