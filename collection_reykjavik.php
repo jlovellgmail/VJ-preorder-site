50b7d7e313490cb400b3184af71e93e4dd125133
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-12-05 12:41:21 -0800

    adjust responsive layout of top text in R collection page

--- collection_reykjavik.php
+++ collection_reykjavik.php
@@ -23,8 +23,7 @@ $seo_variable = "home";
 
 			<div class="topRow">
 				<div class="headline">
-					<div class="part1">Reykjavik</div>
-					<div class="part2">Collection</div>
+					<div class="part1">Reykjavik</div><div class="part2">Collection</div>
 				</div>
 				<div class="text">
 					Reykjavik is about beauty and raw strength. Its volcanic origin belies a region still in formation. This city exhibits a resolve to honor the past and embrace the future. Our selection of materials and original bronze designs are inspired by the unique independence of this remarkable city.
